91a634ab46ef17da33b51605b158df2d66a076aa
RylvinCelnar <rylvincelnar@gmail.com>
2024-11-29 23:41:28 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,83 @@
+import React, { useRef, useState } from 'react';
+import { View, Text, StyleSheet, Button, Animated, Easing, TouchableOpacity } from 'react-native';
+import withLogging from './withLogging';
+import CustomButton from './customButton';
+
+const EnhancedButton = withLogging(CustomButton);
+const App = () => {
+  const [isVisible, setIsVisible] = useState(false); 
+  const fadeAnim = useRef(new Animated.Value(0)).current; 
+  const [colorChanged, setColorChanged] = useState(false); 
+ 
+  const toggleFade = () => {
+    Animated.timing(fadeAnim, {
+      toValue: isVisible ? 0 : 1, 
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+    setIsVisible(!isVisible);
+  };
+ 
+  const translateY = useRef(new Animated.Value(0)).current;
+  const [isMoved, setIsMoved] = useState(false);
+  const buttonColor = colorChanged ? 'green' : 'darkblue';
+
+  const startTranslation = () => {
+    const toValue = isMoved ? 0 : 100;
+    Animated.timing(translateY, {
+      toValue: toValue, 
+      duration: 1000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(); 
+    setIsMoved(!isMoved);   
+  };
+
+  return (
+    <View style={styles.container}>
+
+     <TouchableOpacity
+        style={[styles.button, { backgroundColor: buttonColor}]}
+        onPress={() => {
+          console.log('Enhanced Button Pressed!');
+          setColorChanged(!colorChanged); 
+        }}>
+        <Text style={styles.buttonText}>Enhanced Me</Text>
+      </TouchableOpacity>        
+      <Animated.View style={{ ...styles.box, opacity: fadeAnim,  }}/>
+      <Button title={isVisible ? 'Fade Out' : 'Fade In'} onPress={toggleFade}/>
+ 
+      <Animated.View style={[styles.box, { transform: [{ translateY }] }]}/>
+      <Button title={isMoved ? 'Move Back' : 'Move Down'} onPress={startTranslation}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: 'darkblue',
+    marginBottom: 15, 
+    marginTop: 20,
+    height: 100,
+    width: 100,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    padding: 5,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+});
+
+export default App;
